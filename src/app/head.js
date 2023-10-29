f3be5b02b5856e55b82e659b789d1a117cde0689
tasim313 <mostasimmahmudtasim@gmail.com>
2023-10-29 17:11:45 +0600

    add body section

--- src/app/head.js
+++ src/app/head.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 const Head = () =>{
+
     return(
         <>
-            <title>Google Clone Next js 13</title>
+            <title>Google Clone</title>
             <meta content="width=device-width, initial-scale=1" name="viewport" />
-            <meta name="description" content="Google clone created by Next js 13" />
+            <meta name="description" content="Google clone created by Next js" />
             <link rel="icon" href="/favicon.svg" />
         </>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,25 @@
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
 import React from "react";
 
 const Home = () =>{
+ 
   return(
     <>
+    {/* Header section */}
     <HomeHeader/>
+
+    {/* Body section */}
+    <div className="flex flex-col items-center mt-24">
+        <Image
+         width="300"
+         height="100"
+         src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png'
+         alt="image is not found"
+        />
+        <HomeSearch></HomeSearch>
+    </div>
     </>
   )
 }
