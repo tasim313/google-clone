6a57373d527430e8c312a5177853185ff1445ffd
tasim313 <mostasimmahmudtasim@gmail.com>
2023-10-29 10:16:26 +0600

    add head js file in app dir

---
+++ src/app/head.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Head = () =>{
+    return(
+        <>
+            <title>Google Clone Next js 13</title>
+            <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <meta name="description" content="Google clone created by Next js 13" />
+            <link rel="icon" href="/favicon.svg" />
+        </>
+    )
+}
+
+export default Head;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,18 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from './head'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Google Clone',
-  description: 'For education To Learn Next js',
-}
+// export const metadata = {
+//   title: 'Google Clone',
+//   description: 'For education To Learn Next js',
+// }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head/>
       <body className={inter.className}>{children}</body>
     </html>
   )
