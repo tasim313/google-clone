369cb6eb3f6366041797bce87c54631f08cc58ad
tasim313 <mostasimmahmudtasim@gmail.com>
2023-10-31 17:16:41 +0600

    add pagination component

--- src/components/ImageSearchResults.js
+++ src/components/ImageSearchResults.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import React from "react";
+import PaginationButtons from "./PaginationButtons";
 
 const ImageSearchResults = ({results}) =>{
     return(
         <>
-        <div className="pb-24 mt-4">
+        <div className="sm:pb-24 pb-40 mt-4">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4">
                 {results.items.map((result) =>(
                     <div key={result.link} className="mb-8">
@@ -21,6 +22,9 @@ const ImageSearchResults = ({results}) =>{
                         </div>
                     </div>
                 ))}
+            </div>
+            <div className="ml-16">
+                 <PaginationButtons/>
             </div>
         </div>
         </>
--- src/components/WebSearchResults.js
+++ src/components/WebSearchResults.js
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 import React from 'react'
 import Parser from "html-react-parser"
+import PaginationButtons from './PaginationButtons';
 
 const WebSearchResults = ({results}) =>{
     return(
         <>
-        <div className='w-full mx-auto px-3 pb-24 sm:pl-[5%] md:pl-[14%] lg:pl-52'>
+        <div className='w-full mx-auto px-3 pb-40 sm:pb-24 sm:pl-[5%] md:pl-[14%] lg:pl-52'>
             <p className='text-gray-600 text-sm mb-5 mt-3'>
                 About {results.searchInformation?.formattedTotalResults} results ({results.searchInformation?.formattedSearchTime} seconds)
             </p>
@@ -22,6 +23,7 @@ const WebSearchResults = ({results}) =>{
                   <p className='text-gray-600'>{Parser(result.htmlSnippet)}</p>
                 </div>
             ))}
+            <PaginationButtons/>
         </div>
         </>
     )
