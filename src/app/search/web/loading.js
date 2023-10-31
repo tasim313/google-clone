228171996f15179e9cc0384ac00f16e255b76b25
tasim313 <mostasimmahmudtasim@gmail.com>
2023-10-31 21:41:14 +0600

    add load effect for web and image search

---
+++ src/app/search/web/loading.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const loading =() =>{
+    return(
+        <>
+         <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+            <div className=" h-2.5 w-48 bg-gray-200 rounded-full mb-2.5">
+
+            </div>
+            <div className="h-3.5 w-[360 px] bg-gray-200 rounded-full mb-2.5">
+
+            </div>
+            <div className="h-2 max-w-[560 px] bg-gray-200 rounded-full mb-2.5">
+
+            </div>
+            <div className="h-2 max-w-[530 px] bg-gray-200 rounded-full mb-2.5">
+
+            </div>
+            <div className="h-2 max-w-[530 px] bg-gray-200 rounded-full mb-2.5">
+
+            </div>
+         </div>
+         <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+            <div className=" h-2.5 w-48 bg-gray-200 rounded-full mb-2.5">
+
+            </div>
+            <div className="h-3.5 w-[360 px] bg-gray-200 rounded-full mb-2.5">
+
+            </div>
+            <div className="h-2 max-w-[560 px] bg-gray-200 rounded-full mb-2.5">
+
+            </div>
+            <div className="h-2 max-w-[530 px] bg-gray-200 rounded-full mb-2.5">
+
+            </div>
+            <div className="h-2 max-w-[530 px] bg-gray-200 rounded-full mb-2.5">
+
+            </div>
+         </div>
+         <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+            <div className=" h-2.5 w-48 bg-gray-200 rounded-full mb-2.5">
+
+            </div>
+            <div className="h-3.5 w-[360 px] bg-gray-200 rounded-full mb-2.5">
+
+            </div>
+            <div className="h-2 max-w-[560 px] bg-gray-200 rounded-full mb-2.5">
+
+            </div>
+            <div className="h-2 max-w-[530 px] bg-gray-200 rounded-full mb-2.5">
+
+            </div>
+            <div className="h-2 max-w-[530 px] bg-gray-200 rounded-full mb-2.5">
+
+            </div>
+         </div>
+        </>
+    )
+}
+
+export default loading;
